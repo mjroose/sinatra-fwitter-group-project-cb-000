36ee6d678eae52d2d170dafb39b040986f569ec4
Michael Roose <mjroose@gmail.com>
2018-11-04 18:42:43 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -27,7 +27,11 @@ class TweetsController < ApplicationController
   post '/tweets' do
     if User.logged_in?(session)
       user = User.current_user(session)
+      tweet = Tweet.new(content: params[:content])
+      tweet.user = user
+      tweet.save
       binding.pry
+      redirect to "/tweets/#{tweet.id}"
     else
       redirect to 'login'
     end
