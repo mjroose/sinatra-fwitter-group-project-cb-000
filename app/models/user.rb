95d9bf0934376fc251db682ab3272b038967f35d
Michael Roose <mjroose@gmail.com>
2018-11-04 17:26:31 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ActiveRecord::Base
   validates :username, presence: true
   validates :email, presence: true
   validates :password_digest, presence: true
+
+  def slug
+    username.gsub(/\s/, "-")
+  end
+
+  def self.find_by_slug(slug)
+    username = slug.gsub(/-/, " ")
+    User.find_by(name: )
+  end
 end
