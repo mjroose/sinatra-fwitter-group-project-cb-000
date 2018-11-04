2daee88a32b28b80bfe7289e7f3cf78b85a9fd83
Michael Roose <mjroose@gmail.com>
2018-11-04 18:28:06 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,8 @@ class User < ActiveRecord::Base
   def self.logged_in?(session)
     !!session[:user_id]
   end
+
+  def self.current_user(session)
+    User.find_by(id: session[:user_id])
+  end
 end
